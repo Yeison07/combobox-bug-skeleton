67158e4d834d102016e8094f74f1aabfc5d56a5d
yeison07 <yeisona07@hotmail.com>
2025-07-10 18:02:11 -0500

    navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
   import "../app.css";
 
   let { children } = $props();
 </script>
 
+<header>
+  <button
+    class="btn bg-amber-500 h-1/2"
+    onclick={() => {
+      goto("skeleton");
+    }}>Skeleton test</button
+  >
+
+  <button
+    class="btn bg-green-500 h-1/2"
+    onclick={() => {
+      goto("zig");
+    }}>Zig test</button
+  >
+</header>
 {@render children()}
